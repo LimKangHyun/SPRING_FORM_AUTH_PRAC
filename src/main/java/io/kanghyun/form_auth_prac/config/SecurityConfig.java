d6ea0912ff05004cd6f8425dddea33cd33b6bcdb
limkanghyun <limkh06910691@gmail.com>
2025-04-17 19:34:54 +0900

    feat: BCryptPasswordEncoder 등록 및 URL 권한 기반 Security 설정 추가

---
+++ src/main/java/io/kanghyun/form_auth_prac/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package io.kanghyun.form_auth_prac.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults())
+                .authorizeHttpRequests(
+                        auth ->
+                        {
+                            auth.requestMatchers("/signup", "/signin")
+                                    .anonymous()
+                                .requestMatchers("/user/**")
+                                    .hasAnyAuthority("ADMIN", "MANAGER", "MEMBER")
+                                .requestMatchers("/manager/**")
+                                    .hasAnyAuthority("ADMIN", "MANAGER")
+                                .requestMatchers("/member/**")
+                                    .hasAuthority("MEMBER")
+                                .anyRequest()
+                                    .authenticated();
+                        }
+                )
+                .build();
+    }
+}
