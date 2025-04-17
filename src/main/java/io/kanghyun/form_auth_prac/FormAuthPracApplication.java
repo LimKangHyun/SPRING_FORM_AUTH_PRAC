94ea12093a1f4b88b83253c099a8cee67e86ff75
limkanghyun <limkh06910691@gmail.com>
2025-04-17 17:36:18 +0900

    Initial commit

---
+++ src/main/java/io/kanghyun/form_auth_prac/FormAuthPracApplication.java
@@ -0,0 +1,13 @@
+package io.kanghyun.form_auth_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormAuthPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormAuthPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/kanghyun/form_auth_prac/FormAuthPracApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kanghyun.form_auth_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormAuthPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
