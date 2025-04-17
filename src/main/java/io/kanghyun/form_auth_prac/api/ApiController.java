50e594d552229e027fd875f34dc44aafb0378392
limkanghyun <limkh06910691@gmail.com>
2025-04-17 20:41:00 +0900

    feat: 사용자, 관리자, 매니저 권한에 따른 접근 테스트 API 구현

---
+++ src/main/java/io/kanghyun/form_auth_prac/api/ApiController.java
@@ -0,0 +1,23 @@
+package io.kanghyun.form_auth_prac.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/user")
+    public String user() {
+        return "User!";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "Manager!";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Admin!";
+    }
+}
